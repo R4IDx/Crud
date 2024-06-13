6db880b000c29e9adb860dc8d57463374afe1778
steinerr <robin.steiner@student.reutlingen-university.de>
2024-06-13 11:13:32 +0200

    Basic Strukture

---
+++ my-new-app/pages/page2.js
@@ -0,0 +1,15 @@
+import styles from "./page2.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+       
+      </div>
+    </main>
+  );
+}
