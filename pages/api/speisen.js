2a794f6d9aaab0b3537a2da7ffc8601b5a1a4433
steinerr <robin.steiner@student.reutlingen-university.de>
2024-06-19 09:16:44 +0200

    Vercel Fix 1.5

--- pages/api/speisen.js
+++ pages/api/speisen.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
+  database: process.env.POSTGRES_DATABASE,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   ssl: {
